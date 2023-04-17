156017d6ca7e7047c39d564fa7bc4732d0d28a01
Krystof Sochor <240563@vut.cz>
2023-04-17 09:24:08 +0200

    better selection_sort

--- sorting.py
+++ sorting.py
@@ -23,23 +23,40 @@ def read_data(file_name):
     return data
 
 
-def selection_sort(rada):
-    size = len(rada)
-    for ind in range(size):
-        min_index = ind
+def selection_sort(rada, direction):
+    if direction == "vz":
+        size = len(rada)
+        for ind in range(size):
+            min_index = ind
 
-        for j in range(ind + 1, size):
-            # select the minimum element in every iteration
-            if rada[j] < rada[min_index]:
-                min_index = j
-        # swapping the elements to sort the array
-        (rada[ind], rada[min_index]) = (rada[min_index], rada[ind])
-    return rada
+            for j in range(ind + 1, size):
+                # select the minimum element in every iteration
+                if rada[j] < rada[min_index]:
+                    min_index = j
+            # swapping the elements to sort the array
+            (rada[ind], rada[min_index]) = (rada[min_index], rada[ind])
+        return rada
+    elif direction == "ss":
+        size = len(rada)
+        for ind in range(size):
+            min_index = ind
+
+            for j in range(ind + 1, size):
+                # select the minimum element in every iteration
+                if rada[j] < rada[min_index]:
+                    min_index = j
+            # swapping the elements to sort the array
+            (rada[ind], rada[min_index]) = (rada[min_index], rada[ind])
+        return rada[::-1]
+    else:
+        zprava = "Invalid input"
+        return zprava
+
 
 def main():
     my_data = read_data("numbers.csv")
     print(my_data["series_1"])
-    cisla = selection_sort(my_data["series_1"])
+    cisla = selection_sort(my_data["series_1"], "pp")
     print(cisla)
     pass
 
