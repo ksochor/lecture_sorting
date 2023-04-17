0a48eff3def8b23be447974fcd11ecb959a66086
Krystof Sochor <240563@vut.cz>
2023-04-17 08:48:58 +0200

    nacteni csv

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,9 +10,22 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r", encoding="utf-8") as csv_file:
+        # vykonání operací se souborem v této části
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
 
 def main():
+    my_data = read_data("numbers.csv")
+    print(my_data)
     pass
 
 
